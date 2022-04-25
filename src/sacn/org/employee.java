bdb432e995a402b77da12ae8f97dc9e7fe2ec58a
vickyrio123 <vicky787414@gmail.com>
2022-04-25 19:17:19 +0530

    first commit

---
+++ src/sacn/org/employee.java
@@ -0,0 +1,26 @@
+package sacn.org;
+
+import java.util.Scanner;
+
+public class employee {
+	private void add() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the age");
+		byte age = s.nextByte();
+		System.out.println("enter the name");
+		String name = s.next();
+		
+		System.out.println("enter the phno");
+		long phno = s.nextLong();
+		System.out.println("enter the salary");
+		float sal = s.nextFloat();
+		
+		
+	
+	}
+	public static void main(String[] args) {
+		employee e = new employee();
+		e.add();
+	}
+
+}
